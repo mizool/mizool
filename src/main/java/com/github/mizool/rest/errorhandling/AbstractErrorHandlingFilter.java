d5608e70ce359533c6bf1ad8501546e852370e01
Volker Ivens <volkerivens@incub8.de>
2017-01-19 12:20:22 +0100

    fixes

--- src/main/java/com/github/mizool/rest/errorhandling/AbstractErrorHandlingFilter.java
+++ src/main/java/com/github/mizool/rest/errorhandling/AbstractErrorHandlingFilter.java
@@ -1,3 +1,19 @@
+/**
+ *  Copyright 2017 incub8 Software Labs GmbH
+ *  Copyright 2017 protel Hotelsoftware GmbH
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.github.mizool.rest.errorhandling;
 
 import java.io.IOException;
@@ -15,8 +31,6 @@ import com.github.mizool.rest.core.TransactionalResponseWrapper;
 
 public abstract class AbstractErrorHandlingFilter implements Filter
 {
-    protected abstract byte[] getErrorMessageJsonBytes(ErrorMessageDto errorMessageDto);
-
     @Inject
     private ErrorHandler errorHandler;
 
@@ -45,6 +59,8 @@ public abstract class AbstractErrorHandlingFilter implements Filter
         httpResponse.setStatus(errorResponse.getStatusCode());
         httpResponse.getOutputStream().write(getErrorMessageJsonBytes(errorResponse.getBody()));
     }
+
+    protected abstract byte[] getErrorMessageJsonBytes(ErrorMessageDto errorMessageDto);
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException
--- src/main/java/com/github/mizool/rest/errorhandling/ValidationExceptionMapper.java
+++ src/main/java/com/github/mizool/rest/errorhandling/ValidationExceptionMapper.java
@@ -1,5 +1,22 @@
+/**
+ *  Copyright 2017 incub8 Software Labs GmbH
+ *  Copyright 2017 protel Hotelsoftware GmbH
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.github.mizool.rest.errorhandling;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.validation.ConstraintViolationException;
 import javax.ws.rs.core.MediaType;
@@ -13,9 +30,10 @@ public class ValidationExceptionMapper implements ExceptionMapper<ConstraintViol
 {
     private final ErrorHandler errorHandler;
 
-    public ValidationExceptionMapper()
+    @Inject
+    public ValidationExceptionMapper(ErrorHandler errorHandler)
     {
-        errorHandler = new ErrorHandler();
+        this.errorHandler = errorHandler;
     }
 
     @Override
@@ -23,9 +41,9 @@ public class ValidationExceptionMapper implements ExceptionMapper<ConstraintViol
     {
         ErrorResponse errorResponse = errorHandler.handle(e);
         Response result = Response.status(errorResponse.getStatusCode())
-                                  .type(MediaType.APPLICATION_JSON_TYPE)
-                                  .entity(errorResponse.getBody())
-                                  .build();
+            .type(MediaType.APPLICATION_JSON_TYPE)
+            .entity(errorResponse.getBody())
+            .build();
         return result;
     }
 }
