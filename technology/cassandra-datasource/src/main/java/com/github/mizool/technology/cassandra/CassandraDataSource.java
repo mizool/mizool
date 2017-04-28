ead269e69cb6383e1384da6c586fd972b0f0978d
Volker Ivens <volkerivens@incub8.de>
2017-04-28 15:21:32 +0200

    Added CassandraDataSource

---
+++ technology/cassandra-datasource/src/main/java/com/github/mizool/technology/cassandra/CassandraDataSource.java
@@ -0,0 +1,97 @@
+/**
+ *  Copyright 2017 incub8 Software Labs GmbH
+ *  Copyright 2017 protel Hotelsoftware GmbH
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.github.mizool.technology.cassandra;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ConsistencyLevel;
+import com.datastax.driver.core.QueryOptions;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.mapping.Mapper;
+import com.datastax.driver.mapping.MappingManager;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Iterables;
+
+public class CassandraDataSource
+{
+    @Getter
+    @Setter
+    private String addresses;
+
+    @Getter
+    private Cluster cluster;
+
+    @Getter
+    private Session session;
+
+    private MappingManager mappingManager;
+
+    @PostConstruct
+    public void postConstruct()
+    {
+        this.cluster = Cluster.builder()
+            .addContactPoints(parseAddressString(addresses))
+            .withQueryOptions(getQueryOptions())
+            .withMaxSchemaAgreementWaitSeconds(300)
+            .build()
+            .init();
+        this.session = cluster.connect();
+        this.mappingManager = new MappingManager(session);
+    }
+
+    private String[] parseAddressString(String addresses)
+    {
+        return Iterables.toArray(Splitter.on(",").trimResults().split(addresses), String.class);
+    }
+
+    private QueryOptions getQueryOptions()
+    {
+        QueryOptions queryOptions = new QueryOptions();
+        queryOptions.setConsistencyLevel(ConsistencyLevel.ALL);
+
+        return queryOptions;
+    }
+
+    @PreDestroy
+    public void preDestroy()
+    {
+        closeSession();
+        closeCluster();
+    }
+
+    private void closeSession()
+    {
+        session.close();
+        session = null;
+    }
+
+    private void closeCluster()
+    {
+        cluster.close();
+        cluster = null;
+    }
+
+    public <T> Mapper<T> mapper(Class<T> clazz)
+    {
+        return mappingManager.mapper(clazz);
+    }
+}
