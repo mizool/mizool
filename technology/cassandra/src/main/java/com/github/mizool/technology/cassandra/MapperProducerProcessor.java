4d2b909d298ca01d8841fd2c572b6f60481ee37c
Volker Ivens <volkerivens@incub8.de>
2017-09-19 08:03:07 +0200

    PR fix

--- technology/cassandra/src/main/java/com/github/mizool/technology/cassandra/MapperProducerProcessor.java
+++ technology/cassandra/src/main/java/com/github/mizool/technology/cassandra/MapperProducerProcessor.java
@@ -1,3 +1,19 @@
+/**
+ *  Copyright 2017 incub8 Software Labs GmbH
+ *  Copyright 2017 protel Hotelsoftware GmbH
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.github.mizool.technology.cassandra;
 
 import java.io.IOException;
@@ -38,20 +54,27 @@ public class MapperProducerProcessor extends AbstractProcessor
     }
 
     @Override
-    public synchronized void init(ProcessingEnvironment processingEnv)
+    public synchronized void init(ProcessingEnvironment processingEnvironment)
     {
-        super.init(processingEnv);
-        elementUtils = processingEnv.getElementUtils();
-        filer = processingEnv.getFiler();
+        super.init(processingEnvironment);
+        elementUtils = processingEnvironment.getElementUtils();
+        filer = processingEnvironment.getFiler();
     }
 
     @Override
-    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv)
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnvironment)
     {
-        for (Element annotatedElement : roundEnv.getElementsAnnotatedWith(MapperProducer.class))
+        for (Element annotatedElement : roundEnvironment.getElementsAnnotatedWith(MapperProducer.class))
         {
             verifyElement(annotatedElement);
-            generateCode((TypeElement) annotatedElement, filer);
+            try
+            {
+                generateCode((TypeElement) annotatedElement);
+            }
+            catch (IOException e)
+            {
+                throw new UncheckedIOException(e);
+            }
         }
         return true;
     }
@@ -74,21 +97,13 @@ public class MapperProducerProcessor extends AbstractProcessor
         }
     }
 
-    private void generateCode(TypeElement classElement, Filer filer)
+    private void generateCode(TypeElement classElement) throws IOException
     {
         String mapperProducerClassName = classElement.getQualifiedName() + SUFFIX;
-
-        try
+        JavaFileObject javaFileObject = filer.createSourceFile(mapperProducerClassName);
+        try (Writer writer = javaFileObject.openWriter();)
         {
-            JavaFileObject javaFileObject = filer.createSourceFile(mapperProducerClassName);
-            Writer writer = javaFileObject.openWriter();
             writer.write(assembleStringTemplate(classElement));
-            writer.flush();
-            writer.close();
-        }
-        catch (IOException e)
-        {
-            throw new UncheckedIOException(e);
         }
     }
 
