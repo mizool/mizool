f4fee55fc13d1c85e5310401ecec2fa229cd87c3
Volker Ivens <volkerivens@incub8.de>
2018-01-10 11:52:31 +0100

    PR fixes

--- technology/cassandra/src/main/java/com/github/mizool/technology/cassandra/ClusterProducer.java
+++ technology/cassandra/src/main/java/com/github/mizool/technology/cassandra/ClusterProducer.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2017-2018 incub8 Software Labs GmbH
+ * Copyright 2017-2018 protel Hotelsoftware GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.mizool.technology.cassandra;
 
 import javax.enterprise.inject.Disposes;
@@ -10,18 +26,23 @@ import com.datastax.driver.core.QueryOptions;
 import com.datastax.driver.extras.codecs.jdk8.InstantCodec;
 import com.datastax.driver.extras.codecs.jdk8.LocalDateCodec;
 import com.datastax.driver.extras.codecs.jdk8.LocalTimeCodec;
+import com.github.mizool.core.exception.ConfigurationException;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Iterables;
 
 class ClusterProducer
 {
-    private static final String CASSANDRA_CONTACT_POINTS_PROPERTY_NAME = "mizool.cassandra.contactpoints";
+    private static final String CASSANDRA_CONTACT_POINTS_PROPERTY_NAME = "cassandra.contactpoints";
 
     @Produces
     @Singleton
     public Cluster produce()
     {
-        String addresses = System.getProperty(CASSANDRA_CONTACT_POINTS_PROPERTY_NAME, "127.0.0.1");
+        String addresses = System.getProperty(CASSANDRA_CONTACT_POINTS_PROPERTY_NAME);
+        if (addresses == null)
+        {
+            throw new ConfigurationException("No contact points for cassandra set");
+        }
         Cluster cluster = Cluster.builder()
             .addContactPoints(parseAddressString(addresses))
             .withQueryOptions(getQueryOptions())
@@ -40,6 +61,8 @@ class ClusterProducer
     private QueryOptions getQueryOptions()
     {
         QueryOptions queryOptions = new QueryOptions();
+
+        // Default to the safest consistency level in case a store class does not set one
         queryOptions.setConsistencyLevel(ConsistencyLevel.ALL);
 
         return queryOptions;
