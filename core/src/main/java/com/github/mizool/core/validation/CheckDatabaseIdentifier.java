c5d4ac7864d789ed1e4b2afdd86227eb49f90c09
Volker Ivens <volkerivens@incub8.de>
2018-02-22 15:57:40 +0100

    mandatory flag for validation annotations

--- core/src/main/java/com/github/mizool/core/validation/CheckDatabaseIdentifier.java
+++ core/src/main/java/com/github/mizool/core/validation/CheckDatabaseIdentifier.java
@@ -1,18 +1,18 @@
 /**
- *  Copyright 2017 incub8 Software Labs GmbH
- *  Copyright 2017 protel Hotelsoftware GmbH
+ * Copyright 2017 incub8 Software Labs GmbH
+ * Copyright 2017 protel Hotelsoftware GmbH
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.github.mizool.core.validation;
 
@@ -21,16 +21,28 @@ import javax.validation.ConstraintValidatorContext;
 
 public class CheckDatabaseIdentifier implements ConstraintValidator<DatabaseIdentifier, String>
 {
+    private boolean mandatory;
+
     @Override
     public void initialize(DatabaseIdentifier identifier)
     {
+        mandatory = identifier.mandatory();
     }
 
     @Override
     public boolean isValid(String validationObject, ConstraintValidatorContext constraintValidatorContext)
     {
-        return validationObject == null ||
-            validationObject.isEmpty() ||
-            validationObject.matches("[a-zA-Z][a-zA-Z0-9_]*") && validationObject.length() <= 48;
+        boolean valid = false;
+        if (validationObject != null)
+        {
+            valid = !validationObject.isEmpty() &&
+                validationObject.matches("[a-zA-Z][a-zA-Z0-9_]*") &&
+                validationObject.length() <= 48;
+        }
+        else if (!mandatory)
+        {
+            valid = true;
+        }
+        return valid;
     }
 }
--- core/src/main/java/com/github/mizool/core/validation/CheckEnumValue.java
+++ core/src/main/java/com/github/mizool/core/validation/CheckEnumValue.java
@@ -1,45 +1,54 @@
 /**
- *  Copyright 2017 incub8 Software Labs GmbH
- *  Copyright 2017 protel Hotelsoftware GmbH
+ * Copyright 2017 incub8 Software Labs GmbH
+ * Copyright 2017 protel Hotelsoftware GmbH
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.github.mizool.core.validation;
 
+import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-public class CheckEnumValue extends AbstractTypeMappingValidator<EnumValue>
+public class CheckEnumValue implements ConstraintValidator<EnumValue, String>
 {
     private Class enumeration;
+    private boolean mandatory;
 
     @Override
-    public void initialize(EnumValue annotation)
+    public void initialize(EnumValue enumValue)
     {
-        this.enumeration = annotation.value();
+        this.enumeration = enumValue.value();
+        mandatory = enumValue.mandatory();
     }
 
     @Override
-    protected final boolean isNonNullNonEmptyStringValid(
-        String validationObject, ConstraintValidatorContext constraintValidatorContext)
+    public final boolean isValid(String validationObject, ConstraintValidatorContext constraintValidatorContext)
     {
-        boolean valid = true;
-        try
+        boolean valid = false;
+        if (validationObject != null)
         {
-            Enum.valueOf(enumeration, validationObject);
+            try
+            {
+                Enum.valueOf(enumeration, validationObject);
+                valid = true;
+            }
+            catch (Exception ignored)
+            {
+            }
         }
-        catch (Exception e)
+        else if (!mandatory)
         {
-            valid = false;
+            valid = true;
         }
         return valid;
     }
