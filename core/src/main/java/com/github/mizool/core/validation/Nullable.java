1d46d17ce9486f6a7f576fb470fda7b0cd0a8696
Volker Ivens <volkerivens@incub8.de>
2018-02-27 10:04:45 +0100

    target method needed for lombok annotations

--- core/src/main/java/com/github/mizool/core/validation/Nullable.java
+++ core/src/main/java/com/github/mizool/core/validation/Nullable.java
@@ -17,6 +17,7 @@
 package com.github.mizool.core.validation;
 
 import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
@@ -27,7 +28,7 @@ import java.lang.annotation.Target;
 /**
  * This annotation is the opposite of {@link javax.validation.constraints.NotNull} and is intended to document that a field is optional
  */
-@Target({ FIELD, PARAMETER })
+@Target({ FIELD, PARAMETER, METHOD })
 @Retention(RUNTIME)
 @Documented
 public @interface Nullable
