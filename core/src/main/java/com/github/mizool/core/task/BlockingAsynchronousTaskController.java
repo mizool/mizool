9a367b58cf590659bdfce09ce44ba49be19be2e1
Sven Haberer <schroenser@gmail.com>
2018-03-14 14:51:15 +0100

    Correctly handling InterruptedException. MJX-1746

--- core/src/main/java/com/github/mizool/core/task/BlockingAsynchronousTaskController.java
+++ core/src/main/java/com/github/mizool/core/task/BlockingAsynchronousTaskController.java
@@ -1,18 +1,18 @@
 /**
- *  Copyright 2017 incub8 Software Labs GmbH
- *  Copyright 2017 protel Hotelsoftware GmbH
+ * Copyright 2017-2018 incub8 Software Labs GmbH
+ * Copyright 2017-2018 protel Hotelsoftware GmbH
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.github.mizool.core.task;
 
@@ -21,6 +21,7 @@ import java.util.function.Supplier;
 
 import lombok.RequiredArgsConstructor;
 
+import com.github.mizool.core.exception.UncheckedInterruptedException;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
 
@@ -78,7 +79,8 @@ public class BlockingAsynchronousTaskController
                 }
                 catch (InterruptedException e)
                 {
-                    throw new RuntimeException(e);
+                    Thread.currentThread().interrupt();
+                    throw new UncheckedInterruptedException(e);
                 }
             }
             runningTasks.incrementAndGet();
