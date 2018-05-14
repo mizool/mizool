69224f1c0abd1e188897dc8313699833ac37aa66
Jan Doerrenhaus <jandoerrenhaus@incub8.de>
2018-05-14 09:40:30 +0200

    Reorganized imports.

--- technology/web/src/main/java/com/github/mizool/technology/web/mail/Mail.java
+++ technology/web/src/main/java/com/github/mizool/technology/web/mail/Mail.java
@@ -14,7 +14,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package com.github.mizool.core.mail;
+package com.github.mizool.technology.web.mail;
 
 public interface Mail
 {
