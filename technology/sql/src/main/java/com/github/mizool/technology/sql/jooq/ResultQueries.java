d78d02dba008ac80415ae02d084135bd08104335
Sven Haberer <schroenser@gmail.com>
2020-01-09 08:19:49 +0100

    Removed fragment that was added by overzealous code completion.

--- technology/sql/src/main/java/com/github/mizool/technology/sql/jooq/ResultQueries.java
+++ technology/sql/src/main/java/com/github/mizool/technology/sql/jooq/ResultQueries.java
@@ -46,7 +46,7 @@ public class ResultQueries
     /**
      * @return A live {@code Stream} of the result data. The underlying connection is still open. You have to make sure
      * the {@code Stream} is properly closed, either by <b>always</b> consuming it fully (risky due to exceptions) or
-     * preferrably by consuming it inside a <i>try-with-resources</i></i> block.
+     * preferrably by consuming it inside a <i>try-with-resources</i> block.
      */
     public <R extends TableRecord<R>> Stream<R> list(ResultQuery<R> resultQuery, Table<R> table)
     {
