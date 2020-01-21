a553fb7df43ef5ada7786bc3e8d029af26a62dd3
Jan Dörrenhaus <jandoerrenhaus@incub8.de>
2020-01-21 15:43:33 +0100

    Added behaviour for shiros UnauthorizedException.

---
+++ technology/web/src/main/java/com/github/mizool/technology/web/shiro/UnauthorizedExceptionBehaviour.java
@@ -0,0 +1,48 @@
+package com.github.mizool.technology.web.shiro;
+
+import org.apache.shiro.authz.UnauthorizedException;
+import org.kohsuke.MetaInfServices;
+
+import com.github.mizool.core.rest.errorhandling.ErrorHandlingBehavior;
+import com.github.mizool.core.rest.errorhandling.HttpStatus;
+import com.github.mizool.core.rest.errorhandling.LogLevel;
+
+@MetaInfServices
+public class UnauthorizedExceptionBehaviour implements ErrorHandlingBehavior
+{
+    @Override
+    public Class<? extends Throwable> getThrowableClass()
+    {
+        return UnauthorizedException.class;
+    }
+
+    @Override
+    public boolean includeErrorId()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean includeDetails()
+    {
+        return true;
+    }
+
+    @Override
+    public int getStatusCode()
+    {
+        return HttpStatus.FORBIDDEN;
+    }
+
+    @Override
+    public LogLevel getMessageLogLevel()
+    {
+        return LogLevel.WARN;
+    }
+
+    @Override
+    public LogLevel getStackTraceLogLevel()
+    {
+        return LogLevel.DEBUG;
+    }
+}
