4d554d2be8ccf2bfd1287288b0d9c1963da2d8e3
Jens Bannmann <jensbannmann@incub8.de>
2021-03-25 16:17:43 +0100

    Add remaining docs & improve type names

--- core/src/main/java/com/github/mizool/core/concurrent/Synchronizer.java
+++ core/src/main/java/com/github/mizool/core/concurrent/Synchronizer.java
@@ -12,22 +12,50 @@ import lombok.With;
 import com.github.mizool.core.exception.UncheckedInterruptedException;
 
 /**
- * TODO write docs.
- *
+ * Encapsulates synchronization and wait/notify functionality. <br>
+ * <br>
+ * <h3>Introduction</h3>
+ * By calling any of the instance methods of {@code Synchronizer}, the calling code defines a chain of actions that is
+ * completed with {@code invoke()}. All those actions are performed in one synchronized block using a lock which is
+ * private to the {@code Synchronizer} instance.<br>
+ * <br>
+ * With its fluent API and encapsulated lock, this class can help increasing both the readability and robustness of
+ * concurrent algorithms. Still, care must be taken to avoid deadlocks, just as if using {@code synchronized} blocks
+ * and {@link Object#wait()} / {@link Object#notifyAll()} directly.<br>
+ * <br>
+ * <h3>Actions</h3>
+ * {@code Synchronizer} provides the following actions:<br>
+ * <br>
  * <ul>
- *     <li>define a sequence of actions, all executed inside the same synchronized block</li>
- *     <li>sleep until <i>condition</i><ul>
- *         <li>before/after main action (or both)</li>
- *         <li>evaluated inside the synchronized block when waking; false => sleep again</li>
- *         <li>throws {@link UncheckedInterruptedException}</li>
- *     </ul></li>
- *     <li>"wake others" = {@link Object#notifyAll()}</li>
+ *     <li>Main action
+ *         <ul><li>
+ *             a {@link Runnable} ({@code run}) or {@link Supplier} ({@code get})<br>
+ *         </li></ul><br>
+ *     </li>
+ *     <li>Sleep until <i>condition</i> is {@code true}
+ *         <ul><li>
+ *             Can be used before ({@code sleepUntil}) and/or after ({@code thenSleepUntil}) the main action.<br>
+ *             Sleeping is implemented in line with secure coding practices, i.e. {@link Object#wait()} is called inside
+ *             a loop to ensure liveness and safety (see
+ *             <a href="https://wiki.sei.cmu.edu/confluence/display/java/THI03-J.+Always+invoke+wait%28%29+and+await%28%29+methods+inside+a+loop">SEI
+ *             CERT rule THI03-J</a> for details).<br>
+ *             If the thread is interrupted while waiting, {@link UncheckedInterruptedException} is thrown and the
+ *             thread's interrupted flag is re-set.<br>
+ *         </li></ul><br>
+ *     </li>
+ *     <li>Wake other threads</li>
+ *         <ul><li>
+ *             Using {@code wakeOthers()} is equivalent to calling {@link Object#notifyAll()}. If the main action is a
+ *     {@code Supplier<T>}, the calling code can decide whether waking takes place by using
+ *     {@code andWakeOthersIf(Predicate<T>)} instead.<br>
+ *         </li></ul><br>
+ *     </li>
  * </ul>
- *
- * <img src="doc-files/synchronizer-railroad-diagram.svg">
- *
- * <pre>{@code
- * synchronizer.run(...)
+ * <h3>Fluent API overview</h3>
+ * <img src="doc-files/synchronizer-railroad-diagram.svg"><br>
+ * <br>
+ * <h3>Example usage</h3>
+ * <pre>{@code synchronizer.run(...)
  *     .thenSleepUntil(...)
  *     .invoke();
  *
@@ -43,11 +71,11 @@ import com.github.mizool.core.exception.UncheckedInterruptedException;
  *     .andWakeOthersIf(Spline::isReticulated)
  *     .invoke();}</pre>
  */
-public final class Synchronizer implements SynchronizerDsl.Pre
+public final class Synchronizer implements SynchronizerApi.SleepRunGet
 {
     @With
     @Builder
-    private static final class RunAction implements SynchronizerDsl.RunPostMayWake
+    private static final class RunAction implements SynchronizerApi.Run.WakeSleepInvoke
     {
         @NonNull
         private final Lock lock;
@@ -80,13 +108,13 @@ public final class Synchronizer implements SynchronizerDsl.Pre
         }
 
         @Override
-        public SynchronizerDsl.RunPostMaySleep andWakeOthers()
+        public SynchronizerApi.Run.SleepInvoke andWakeOthers()
         {
             return withWakeOthers(true);
         }
 
         @Override
-        public SynchronizerDsl.RunInvokable thenSleepUntil(BooleanSupplier state)
+        public SynchronizerApi.Run.Invoke thenSleepUntil(@NonNull BooleanSupplier state)
         {
             return withPostState(state);
         }
@@ -94,7 +122,7 @@ public final class Synchronizer implements SynchronizerDsl.Pre
 
     @With
     @Builder
-    private static final class GetAction<T> implements SynchronizerDsl.GetPostMayWake<T>
+    private static final class GetAction<T> implements SynchronizerApi.Get.WakeSleepInvoke<T>
     {
         @NonNull
         private final Lock lock;
@@ -129,26 +157,26 @@ public final class Synchronizer implements SynchronizerDsl.Pre
         }
 
         @Override
-        public SynchronizerDsl.GetPostMaySleep<T> andWakeOthers()
+        public SynchronizerApi.Get.SleepInvoke<T> andWakeOthers()
         {
             return andWakeOthersIf(t -> true);
         }
 
         @Override
-        public SynchronizerDsl.GetPostMaySleep<T> andWakeOthersIf(Predicate<T> predicate)
+        public SynchronizerApi.Get.SleepInvoke<T> andWakeOthersIf(@NonNull Predicate<T> predicate)
         {
             return withWakeOthersPredicate(predicate);
         }
 
         @Override
-        public SynchronizerDsl.GetInvokable<T> thenSleepUntil(BooleanSupplier state)
+        public SynchronizerApi.Get.Invoke<T> thenSleepUntil(@NonNull BooleanSupplier state)
         {
             return withPostState(state);
         }
     }
 
     @Builder
-    private static class ActionChoice implements SynchronizerDsl.Action
+    private static class RunGetChoice implements SynchronizerApi.RunGet
     {
         @NonNull
         protected final Lock lock;
@@ -156,7 +184,7 @@ public final class Synchronizer implements SynchronizerDsl.Pre
         private final BooleanSupplier preState;
 
         @Override
-        public SynchronizerDsl.RunPostMayWake run(Runnable runnable)
+        public SynchronizerApi.Run.WakeSleepInvoke run(@NonNull Runnable runnable)
         {
             return RunAction.builder()
                 .lock(lock)
@@ -166,7 +194,7 @@ public final class Synchronizer implements SynchronizerDsl.Pre
         }
 
         @Override
-        public <T> SynchronizerDsl.GetPostMayWake<T> get(Supplier<T> getter)
+        public <T> SynchronizerApi.Get.WakeSleepInvoke<T> get(@NonNull Supplier<T> getter)
         {
             return GetAction.<T>builder().lock(lock)
                 .preState(preState)
@@ -180,6 +208,7 @@ public final class Synchronizer implements SynchronizerDsl.Pre
     {
         protected void sleepUntil(BooleanSupplier state)
         {
+            // TODO get rid of this: add @NonNull to state param and fields, use ()->true instead
             if (state == null)
             {
                 return;
@@ -205,32 +234,40 @@ public final class Synchronizer implements SynchronizerDsl.Pre
         }
     }
 
-    private final Lock lock = new Lock();
+    private final Lock lock;
+
+    /**
+     * Creates a new {@code Synchronizer} instance.
+     */
+    public Synchronizer()
+    {
+        lock = new Lock();
+    }
 
     @Override
-    public SynchronizerDsl.RunPostMayWake run(Runnable runnable)
+    public SynchronizerApi.Run.WakeSleepInvoke run(@NonNull Runnable runnable)
     {
-        return actionChoiceBuilder().build()
+        return runGetChoiceBuilder().build()
             .run(runnable);
     }
 
     @Override
-    public <T> SynchronizerDsl.GetPostMayWake<T> get(Supplier<T> getter)
+    public <T> SynchronizerApi.Get.WakeSleepInvoke<T> get(@NonNull Supplier<T> getter)
     {
-        return actionChoiceBuilder().build()
+        return runGetChoiceBuilder().build()
             .get(getter);
     }
 
     @Override
-    public SynchronizerDsl.Action sleepUntil(BooleanSupplier state)
+    public SynchronizerApi.RunGet sleepUntil(@NonNull BooleanSupplier state)
     {
-        return actionChoiceBuilder().preState(state)
+        return runGetChoiceBuilder().preState(state)
             .build();
     }
 
-    private ActionChoice.ActionChoiceBuilder actionChoiceBuilder()
+    private RunGetChoice.RunGetChoiceBuilder runGetChoiceBuilder()
     {
-        return ActionChoice.builder()
+        return RunGetChoice.builder()
             .lock(lock);
     }
 }
---
+++ core/src/main/java/com/github/mizool/core/concurrent/SynchronizerApi.java
@@ -0,0 +1,163 @@
+package com.github.mizool.core.concurrent;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+/**
+ * Provides fluent syntax for for {@link Synchronizer}.
+ */
+public interface SynchronizerApi
+{
+    /*
+     * Note on terminology: this API intentionally uses the verbs "sleep" and "wake". If it used "wait" and "notify"
+     * instead, its methods would all too easily be confused with methods in java.lang.Object which, if invoked on the
+     * objects returned by chained methods, could cause deadlocks.
+     */
+
+    interface SleepRunGet extends RunGet
+    {
+        /**
+         * Adds sleeping to the action chain. <br>
+         * <br>
+         * When performing this action, the chain will sleep until another chain wakes it up, then call the given
+         * supplier. If the supplier returns {@code false}, this action chain resumes sleeping. Otherwise, the chain
+         * continues by performing the main action.
+         *
+         * @param state the supplier that returns {@code true} if the action chain should continue, {@code false}
+         * otherwise.
+         *
+         * @throws NullPointerException if {@code state} is null
+         */
+        RunGet sleepUntil(BooleanSupplier state);
+    }
+
+    interface RunGet
+    {
+        /**
+         * Sets the given {@link Runnable} as the main action of the chain.
+         *
+         * @param runnable the action to perform
+         *
+         * @throws NullPointerException if {@code runnable} is null
+         */
+        Run.WakeSleepInvoke run(Runnable runnable);
+
+        /**
+         * Sets the given {@link Supplier} as the main action of the chain.
+         *
+         * @param getter the action to perform
+         *
+         * @throws NullPointerException if {@code getter} is null
+         */
+        <T> Get.WakeSleepInvoke<T> get(Supplier<T> getter);
+    }
+
+    interface Run
+    {
+        interface WakeSleepInvoke extends SleepInvoke, Base.WakeSleepInvoke
+        {
+            @Override
+            SleepInvoke andWakeOthers();
+        }
+
+        interface SleepInvoke extends Invoke, Base.SleepInvoke
+        {
+            @Override
+            Invoke thenSleepUntil(BooleanSupplier state);
+        }
+
+        interface Invoke
+        {
+            /**
+             * Invokes the action chain in a synchronized block. <br>
+             * <br>
+             * All actions will be performed in order. This method blocks until all actions in the chain have been
+             * completed.<br>
+             * <br>
+             * While several action chains can be invoked concurrently on the same synchronizer, only one of them will
+             * perform an action at any given time. As this involves acquiring a lock shared with other action chains,
+             * care must be taken to avoid deadlocks, just as if using {@code synchronized} blocks and
+             * {@link Object#wait()} / {@link Object#notifyAll()} directly.
+             *
+             * @throws com.github.mizool.core.exception.UncheckedInterruptedException if the thread was interrupted
+             * while waiting (i.e. performing a sleep action).
+             */
+            void invoke();
+        }
+    }
+
+    interface Get
+    {
+        interface WakeSleepInvoke<T> extends SleepInvoke<T>, Base.WakeSleepInvoke
+        {
+            @Override
+            SleepInvoke<T> andWakeOthers();
+
+            /**
+             * Adds an action which conditionally wakes other chains.
+             *
+             * @param predicate a test for the result of the main action that returns {@code true} if this action chain
+             * should wake other chains, {@code false} otherwise.
+             *
+             * @throws NullPointerException if {@code predicate} is null
+             */
+            SleepInvoke<T> andWakeOthersIf(Predicate<T> predicate);
+        }
+
+        interface SleepInvoke<T> extends Invoke<T>, Base.SleepInvoke
+        {
+            @Override
+            Invoke<T> thenSleepUntil(BooleanSupplier state);
+        }
+
+        interface Invoke<T>
+        {
+            /**
+             * Invokes the action chain in a synchronized block. <br>
+             * <br>
+             * All actions will be performed in order. This method blocks until all actions in the chain have been
+             * completed.<br>
+             * <br>
+             * While several action chains can be invoked concurrently on the same synchronizer, only one of them will
+             * perform an action at any given time. As this involves acquiring a lock shared with other action chains,
+             * care must be taken to avoid deadlocks, just as if using {@code synchronized} blocks and
+             * {@link Object#wait()} / {@link Object#notifyAll()} directly.
+             *
+             * @return the object returned by the main action
+             *
+             * @throws com.github.mizool.core.exception.UncheckedInterruptedException if the thread was interrupted
+             * while waiting (i.e. performing a sleep action).
+             */
+            T invoke();
+        }
+    }
+
+    interface Base
+    {
+        interface WakeSleepInvoke extends SleepInvoke
+        {
+            /**
+             * Adds an action which wakes other chains.
+             */
+            Object andWakeOthers();
+        }
+
+        interface SleepInvoke
+        {
+            /**
+             * Adds sleeping to the end of the action chain. <br>
+             * <br>
+             * When performing this action, the chain will sleep until another chain wakes it up, then call the given
+             * supplier. If the supplier returns {@code false}, this action chain resumes sleeping. Otherwise, the chain
+             * invocation finishes.
+             *
+             * @param state the supplier that returns {@code true} if the chain invocation should finish, {@code false}
+             * otherwise.
+             *
+             * @throws NullPointerException if {@code state} is null
+             */
+            Object thenSleepUntil(BooleanSupplier state);
+        }
+    }
+}
