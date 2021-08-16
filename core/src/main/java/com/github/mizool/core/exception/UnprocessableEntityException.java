911e187f97b292ef6cc4688b2d619d261138f7f3
Matej Jelusic <matej.jelusic@protel.net>
2021-08-16 14:59:24 +0200

    Make UnprocessableEntityException deprecated with appropriate message in javadoc

--- core/src/main/java/com/github/mizool/core/exception/UnprocessableEntityException.java
+++ core/src/main/java/com/github/mizool/core/exception/UnprocessableEntityException.java
@@ -1,8 +1,11 @@
 package com.github.mizool.core.exception;
 
 /**
- * Thrown when the entity is syntactically correct (e.g. all non-null properties are set), but could not be processed
- * for semantic reasons (e.g. invalid references to other database entities).
+ * @deprecated It is designed to be thrown when the entity is syntactically correct (e.g. all non-null properties are set),
+ * but could not be processed for semantic reasons (e.g. invalid references to other database entities).
+ * But the UnprocessableEntityException doesn't really say why the entity is unprocessable and
+ * is generally a bad fit as no client ever really has a chance of handling such a response. Try to avoid using this
+ * exception and use more specific one.
  */
 public class UnprocessableEntityException extends RuntimeException
 {
