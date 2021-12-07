0dde9abfde9cb97dc3baa11704e23201fc4c0d67
Sven Haberer <svenhaberer@incub8.de>
2021-12-07 09:41:15 +0100

    Addressing review comments,

--- core/src/main/java/com/github/mizool/core/Interval.java
+++ core/src/main/java/com/github/mizool/core/Interval.java
@@ -2,13 +2,16 @@ package com.github.mizool.core;
 
 import java.time.Duration;
 
+import javax.annotation.concurrent.ThreadSafe;
+
 import lombok.RequiredArgsConstructor;
+import lombok.Synchronized;
 
 import com.google.common.base.Stopwatch;
 
 /**
- * Helper class to allow for something to happen after a certain amount of time. A common uses cases might be
- * invalidation of in memory data.<br>
+ * Helper class to allow for something to happen at regular intervals. A common use case might be invalidation of
+ * in-memory data.<br>
  * <br>
  * Example to invalidate a cache every minute:
  * <pre>
@@ -17,26 +20,32 @@ import com.google.common.base.Stopwatch;
  *
  * ...
  *
- * if (cacheInvalidationInterval.isReached())
- * {
- *     invalidateCache();
- * }
+ * cacheInvalidationInterval.runIfDue(this::invalidateCache);
  * }
  * </pre>
  */
+@ThreadSafe
 @RequiredArgsConstructor
 public class Interval
 {
     private final Stopwatch stopwatch = Stopwatch.createStarted();
     private final Duration duration;
 
-    public boolean isReached()
+    /**
+     * Executes the {@code runnable} if the interval is due or <i>past due</i>. The next full interval is started after
+     * completion of the runnable.
+     *
+     * @return {@code true} if the operation was due and the runnable completed.
+     */
+    @Synchronized
+    public boolean runIfDue(Runnable runnable)
     {
         boolean isReached = stopwatch.elapsed()
             .compareTo(duration) > 0;
         if (isReached)
         {
             stopwatch.reset();
+            runnable.run();
             stopwatch.start();
         }
         return isReached;
