ecf3fae99e500462d238141352769a8367cdfa31
Jens Bannmann <jens.b@web.de>
2026-01-18 13:39:42 +0100

    Add Identifiable.obtainId()

--- core/src/main/java/com/github/mizool/core/Identifiable.java
+++ core/src/main/java/com/github/mizool/core/Identifiable.java
@@ -10,4 +10,20 @@ public interface Identifiable<T extends Identifiable<T>>
      * @return the ID, may be {@code null} (e.g. because the Identifiable is in the process of being created)
      */
     @Nullable Identifier<T> getId();
+
+    /**
+     * @return the non-null ID
+     *
+     * @throws IllegalStateException if the ID is null, e.g. because the Identifiable is in the process of being created
+     */
+    default Identifier<T> obtainId()
+    {
+        var id = getId();
+        if (id == null)
+        {
+            throw new IllegalStateException();
+        }
+
+        return id;
+    }
 }
